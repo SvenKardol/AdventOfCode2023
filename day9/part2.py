7bbf7d456dd4f1280aab96f4ccbb26214fb4bf82
SvenKardol <svenkardol@hotmail.com>
2023-12-09 08:29:00 +0100

    added day 9

---
+++ day9/part2.py
@@ -0,0 +1,36 @@
+from aocd import get_data
+
+dataRaw = get_data(year=2023, day=9)
+
+data = dataRaw.split("\n")
+
+
+def determine_diff_set(nums):
+    return_set = [nums]
+    while True:
+        diff = [return_set[-1][i + 1] - return_set[-1][i] for i in range(len(return_set[-1]) - 1)]
+        return_set.append(diff)
+
+        if all([v == 0 for v in diff]):
+            break
+
+    return return_set
+
+
+total = 0
+for line in data:
+    original_nums = [int(num) for num in line.split()]
+
+    diff_set = determine_diff_set(original_nums)
+
+    while len(diff_set) > 1:
+        sequence_increment = diff_set[-1][0]
+        diff_set.remove(diff_set[-1])
+
+        # prepend found value to the last diff_set
+        diff_set[-1].insert(0, diff_set[-1][0] - sequence_increment)
+
+    # add first element of each set to the total
+    total += diff_set[0][0]
+
+print(total)
